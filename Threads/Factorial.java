da07047827554bd6e174a883a7174650ab85ae6a
LUCA LENCIONI <luca.lencioni09@gmail.com>
2022-10-18 17:33:31 +0200

    Ultrasuca

--- Threads/Factorial.java
+++ Threads/Factorial.java
@@ -1,10 +1,13 @@
 package Threads;
 
+import org.w3c.dom.ranges.Range;
+
 public class Factorial extends Thread {
-    public int number;
-    public int factored;
-    public static int[][] matrix = new int[2][10];
-    public static int index = 0;
+    public static final int RANGE = 10;
+    private int number;
+    private long factored;
+    private static long[][] matrix = new long[2][RANGE];
+    private static int index = 0;
 
     public Factorial(int number) {
         this.number = number;
@@ -17,12 +20,16 @@ public class Factorial extends Thread {
         synchronized (Factorial.class) {
             matrixInsert();
             System.out.println(matrix[0][index - 1] + " -> " + matrix[1][index - 1]);
-        }
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
 
+            }
+        }
     }
 
-    private static int factorial(int n) {
-        int fact = 1;
+    private static long factorial(int n) {
+        long fact = 1;
         for (int i = 2; i <= n; i++) {
             fact = fact * i;
 
@@ -32,11 +39,8 @@ public class Factorial extends Thread {
     }
 
     private synchronized void matrixInsert() {
-        if (index != 10) {
-            matrix[0][index] = this.number;
-            matrix[1][index] = this.factored;
-            index++;
-        } else
-            System.err.println("Kitammurt");
+        matrix[0][index] = this.number;
+        matrix[1][index] = this.factored;
+        index++;
     }
 }
--- Threads/Main.java
+++ Threads/Main.java
@@ -6,25 +6,27 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\033[H\033[2J");
+        System.out.print("\033[H\033[2J");
         System.out.flush();
-        randomizer(7);
+        randomizer(5);
     }
 
     public static void randomizer(int threads) {
-        int size = 10;
 
-        ArrayList<Integer> list = new ArrayList<Integer>(size);
-        for (int i = 1; i <= size; i++) {
-            list.add(i);
+        if (threads >= Factorial.RANGE) {
+            throw new IndexOutOfBoundsException(
+                    "\n No duplicates idiot, Number of Threads must be under or equal to RANGE");
         }
 
+        ArrayList<Integer> list = new ArrayList<Integer>(Factorial.RANGE);
         Random rand = new Random();
-        while (list.size() > (10 - threads)) {
-            int index = rand.nextInt(list.size());
-            Factorial t = new Factorial(list.remove(index));
-            t.start();
 
+        for (int i = 1; i <= Factorial.RANGE; i++) {
+            list.add(i);
+        }
+        while (list.size() > (Factorial.RANGE - threads)) {
+            int index = rand.nextInt(list.size());
+            new Factorial(list.remove(index)).start();
         }
 
     }
