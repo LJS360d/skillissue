ae262fcb8168e017922f1286cf6976bc077aee18
LUCA LENCIONI <luca.lencioni09@gmail.com>
2022-11-07 06:04:54 +0100

    pog

--- Threads/Cartoleria/src/Cliente.java
+++ Threads/Cartoleria/src/Cliente.java
@@ -1,55 +1,52 @@
 public class Cliente extends Thread{
-        private int wantPencil;
-        private int wantNotebook;
-        
-        public Cliente(int wantPencil, int wantNotebook) {
-            super();
-            this.wantPencil = wantPencil;
-            this.wantNotebook = wantNotebook;
-        }
-
-        @Override
-        public void run() {
+    private int wantPencil;
+    private int wantNotebook;
+    
+    public Cliente(int wantPencil, int wantNotebook) {
+        super();
+        this.wantPencil = wantPencil;
+        this.wantNotebook = wantNotebook;
+    }
+    @Override
+    public void run() {
+    if(!claimPencils())System.out.println(this.getName()+" ha sucato");
+    if(!claimNotebooks())System.out.println(this.getName()+" ha sucato");
+    }
+    
+        public boolean claimPencils(){
         System.out.println(this.getName()+"-Voglio "+wantPencil+" Matite");
         while(wantPencil > 0){
             synchronized(Cartoleria.class){
-                if(Cartoleria.pencils > 0){
-                    Cartoleria.pencils--;
-                    wantPencil--;
-                }
-                else{
-                    System.out.println(Thread.currentThread().getName()+" Mi mancano "+wantPencil+ " Matite");
+                if(Cartoleria.pencils <= 0){
+                    System.out.println(this.getName()+" Mi mancano "+wantPencil+ " Matite");
                     wantPencil=-1;
-                    System.err.println("Cartoleria- Puppa");
                     break;
                 }
+                    Cartoleria.pencils--;
+                    wantPencil--;
             }
         }  
-        if (wantPencil > -1){
-            System.out.println(this.getName()+" Poggers");
-        } else System.out.println(this.getName()+" Per niente poggers");
+        if (wantPencil == -1){
+            return false;
+        } return true;
+        }
 
+        public boolean claimNotebooks(){
         System.out.println(this.getName()+"-Voglio "+wantNotebook+" Quaderni");
         while(wantNotebook > 0){
             synchronized(Cartoleria.class){
-                if(Cartoleria.notebooks>0){
-                    Cartoleria.notebooks--;
-                   wantNotebook--;
-                }
-                else{
-                    System.out.println(Thread.currentThread().getName()+" Mi mancano "+wantNotebook+ " Quaderni");
-                    System.err.println("Cartoleria- Puppa");
+                if(Cartoleria.notebooks<=0){
+                    System.out.println(this.getName()+" Mi mancano "+wantNotebook+ " Quaderni");
                     wantNotebook=-1;
                     break;
                 }
+                Cartoleria.notebooks--;
+                wantNotebook--;
             }    
         }
-        if(wantNotebook > -1){
-            System.out.println(this.getName()+" Molto Poggers"); 
-        } else System.out.println(this.getName()+" Napoli");
-               
+        if(wantNotebook == -1){
+        return false;
+        } 
+        return true;
         }
-
-
-        
-}
+}   
